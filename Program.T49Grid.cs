c3d18acb71aee6ac6a2730565ad5490d3a40b91f
noclearreaction <noclearreaction@tunnel49.net>
2022-04-07 12:18:41 +0200

    Add project files.

---
+++ Program.T49Grid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class T49Grid
+        {
+            private readonly IMyProgrammableBlock me;
+            private readonly string gridname;
+
+            public T49Grid(IMyProgrammableBlock me, MyIni ini)
+            {
+                this.me = me;
+                this.gridname = ini.Get("T49Grid", "Name").ToString("Undefined");
+            }
+            public void Tick100()
+            {
+                if (me.CubeGrid.CustomName.StartsWith("SmallGrid ") ||
+                    me.CubeGrid.CustomName.StartsWith("LargeGrid ") )
+                {
+                    me.CubeGrid.CustomName = gridname;
+                }
+                // Initialize Groups?
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        private readonly MyIni _ini = new MyIni();
+        private T49Grid _grid;
+
+        public Program()
+        {
+            MyIniParseResult result;
+            if (!_ini.TryParse(Me.CustomData, out result))
+                throw new Exception(result.ToString());
+            _grid = new T49Grid(Me,_ini);
+                
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+        }
+
+        public void Save()
+        {
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            switch (updateSource) {
+                case UpdateType.Update100:
+                    _grid.Tick100();
+                    break;
+                case UpdateType.Update10:
+                case UpdateType.Update1:
+                    break;
+                default:
+                    // throw new NotImplementedException();
+                    break;
+            }
+            return;
+        }
+    }
+}
